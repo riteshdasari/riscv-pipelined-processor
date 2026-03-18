cc1d7ea00b7ee678cda724111cb5905425f9c102
riteshdasari <riteshdasari04@gmail.com>
2026-03-18 08:50:36 +0530

    Implement pc module: supports enable and reset

---
+++ rtl/pc.sv
@@ -0,0 +1,17 @@
+module pc(
+    input logic clk,
+    input logic enb,
+    input logic [31:0]nx_pc,
+    input logic rst_n,
+    output logic [31:0] pc
+);
+always_ff @(posedge clk) begin
+    if(!rst_n){
+        pc<=0;
+    }else if(enb==1){
+        pc<=nx_pc;
+    }else{
+        pc<=pc;
+    }
+end
+endmodule
